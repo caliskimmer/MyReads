061cf6388aaca15679bd60964a79b6fa19ba2362
caliskimmer <caliskimmer@gmail.com>
2019-05-09 10:02:11 -0700

    Moved BookShelf and BookLibrary to standalone functions as they are stateless

--- src/components/BookLibrary.js
+++ src/components/BookLibrary.js
@@ -4,49 +4,47 @@ import PropTypes from 'prop-types'
 
 import BookShelf from './BookShelf'
 
-class BookLibrary extends React.Component {
-    render() {
-        let { books, updateShelf } = this.props;
+const BookLibrary = (props) => {
+    const { books, updateShelf } = props;
 
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <BookShelf
-                            title="Currently Reading"
-                            books={books.filter((book) => (
-                                book.shelf === 'currentlyReading'
-                            ))}
-                            updateShelf={updateShelf}
-                        />
-                        <BookShelf
-                            title="Want to Read"
-                            books={books.filter((book) => (
-                                book.shelf === 'wantToRead'
-                            ))}
-                            updateShelf={updateShelf}
-                        />
-                        <BookShelf
-                            title="Read"
-                            books={books.filter((book) => (
-                                book.shelf === 'read'
-                            ))}
-                            updateShelf={updateShelf}
-                        />
-                    </div>
-                </div>
-                <Link
-                    className="open-search"
-                    to="/search">
-                    Add a book
-                </Link>
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
             </div>
-        )
-    }
-}
+            <div className="list-books-content">
+                <div>
+                    <BookShelf
+                        title="Currently Reading"
+                        books={books.filter((book) => (
+                            book.shelf === 'currentlyReading'
+                        ))}
+                        updateShelf={updateShelf}
+                    />
+                    <BookShelf
+                        title="Want to Read"
+                        books={books.filter((book) => (
+                            book.shelf === 'wantToRead'
+                        ))}
+                        updateShelf={updateShelf}
+                    />
+                    <BookShelf
+                        title="Read"
+                        books={books.filter((book) => (
+                            book.shelf === 'read'
+                        ))}
+                        updateShelf={updateShelf}
+                    />
+                </div>
+            </div>
+            <Link
+                className="open-search"
+                to="/search">
+                Add a book
+            </Link>
+        </div>
+    )
+};
 
 BookLibrary.propTypes = {
     books: PropTypes.array,
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -3,31 +3,29 @@ import PropTypes from 'prop-types'
 
 import Book from './Book'
 
-class BookShelf extends React.Component {
-    render() {
-        let { books, updateShelf } = this.props;
+const BookShelf = (props) => {
+    let { books, updateShelf, title } = props;
 
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
-                <ol className="books-grid">
-                    {books.map((book) => (
-                        <li key={book.id}>
-                            <Book
-                                title={book.title}
-                                cover={(book.imageLinks && book.imageLinks.thumbnail) || ''}
-                                authors={book.authors || []}
-                                shelf={book.shelf}
-                                id={book.id}
-                                updateShelf={updateShelf.bind(null, book)}
-                            />
-                        </li>
-                    ))}
-                </ol>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <ol className="books-grid">
+                {books.map((book) => (
+                    <li key={book.id}>
+                        <Book
+                            title={book.title}
+                            cover={(book.imageLinks && book.imageLinks.thumbnail) || ''}
+                            authors={book.authors || []}
+                            shelf={book.shelf}
+                            id={book.id}
+                            updateShelf={updateShelf.bind(null, book)}
+                        />
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
+};
 
 BookShelf.propTypes = {
     title: PropTypes.string.isRequired,
